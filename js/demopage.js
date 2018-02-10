5213b8ff8bc71bc416898e7b5200541e76c3c4fa
somesh-13 <32263694+somesh-13@users.noreply.github.com>
2018-02-10 23:20:42 +0530

    Update demopage.js
    animation overlay effect change

--- js/demopage.js
+++ js/demopage.js
@@ -1,5 +1,5 @@
 var openNav =	function() {
-        document.getElementById("myNav").style.width = "100%";
+     document.getElementById("myNav").style.height = "100%";
     }
      openNav();
     
