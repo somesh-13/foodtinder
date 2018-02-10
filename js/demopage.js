b9a8626156ab1ffc13a2b6544496fe140960386c
somesh-13 <32263694+somesh-13@users.noreply.github.com>
2018-02-10 17:29:22 +0530

    Add files via upload
    demo page js added

---
+++ js/demopage.js
@@ -0,0 +1,9 @@
+var openNav =	function() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+     openNav();
+    
+  function closeNavCLick () {
+        document.getElementById("myNav").style.width = "0%";
+    }
+    closeNavCLick();
